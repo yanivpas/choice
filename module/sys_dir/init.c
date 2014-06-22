cb518e465ff66ece2328eeb5ed42d61c61d799e0
ofir arias <ofirarias@gmail.com>
2014-06-22 20:14:57 +0300

    Fixing "module/sys_dir/init.c" to compile correctly

--- module/sys_dir/init.c
+++ module/sys_dir/init.c
@@ -7,28 +7,15 @@
 
 static struct proc_dir_entry* sys_dir;
 
-static const struct file_operations sys_dir_fops = {
-    .owner = THIS_MODULE,
-    .open = sys_dir_open,
-    .read = sys_dir_read,
-    .write = sys_dir_write,
-};
-
-static ssize_t sys_dir_read(struct file *file, char __user *, size_t, loff_t *)
+static ssize_t sys_dir_write(struct file *file, const char *buf, size_t count, loff_t *pos)
 {
     //TODO
     return 0;
 }
 
-static int sys_dir_write(struct file *file, const char *buf, size_t count, loff_t *pos)
+static int sys_dir_show(struct seq_file *m, void *v)
 {
-    //TODO
-    return 0;
-}
-
-static int sys_dir_show(struct seq_file, *m, void *v)
-{
-    seq_printf(m, "choice modulce is alive! cmf");
+    seq_printf(m, "choice modulce is alive! cmf\n");
     return 0;
 }
 
@@ -37,11 +24,21 @@ static int sys_dir_open(struct inode *inode, struct file *file)
     return single_open(file, sys_dir_show, NULL);
 }
 
+static const struct file_operations sys_dir_fops = {
+    .owner = THIS_MODULE,
+    .open = sys_dir_open,
+    .read = seq_read,
+    .write = sys_dir_write,
+    .llseek = seq_lseek,
+    .release = single_release,
+};
+
+
 int sys_dir_init(void)
 {
-    choice_file = proc_create("choice_module", 0, NULL, &choice_fops);
+    sys_dir = proc_create("choice_module", 0, NULL, &sys_dir_fops);
 
-    if (!choice_fops) {
+    if (!sys_dir) {
         return -ENOMEM;
     }
 
